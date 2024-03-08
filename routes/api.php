604984dc80678d145a7e317f533be869a65c2184
alexfigueirinha <alex.figueirinha@sp.senai.br>
2024-03-08 11:06:15 -0300

    Correção Commit

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('receber/nome', function (request $request) {
     $nome = $request->input('nome');
     return $nome;
+<<<<<<< HEAD
 });
 
 Route::get('receber/nome/idade', function (request $request) {
     $nome = $request->input('nome');
     $idade = $request->input('idade');
     return 'Meu nome é ' . $nome . ', tenho ' . $idade . ' anos';
+=======
+>>>>>>> f5c4cd6 (Exercio 1)
 });
